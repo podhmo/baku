e434767c2056b97cabd970bbbb712413ad6496c5
podhmo <ababjam61+github@gmail.com>
2020-03-24 00:32:39 +0900

    tests: more tests

--- baku/tests/test_q.py
+++ baku/tests/test_q.py
@@ -1,8 +1,47 @@
+import pytest
 from functools import partial
-from baku.q import q, QBuilder
 
 
-def test_it():
-    builder = QBuilder()
-    q_ = partial(q, builder=builder)
-    assert str(q_("x")) == "x"
+@pytest.fixture(scope="module")
+def q():
+    from baku.q import q, QBuilder
+
+    return partial(q, builder=QBuilder())
+
+
+def test_simple(q):
+    assert str(q("x")) == "x"
+
+
+def test_and(q):
+    assert str(q("x").And(q("y"))) == "(x and y)"
+
+
+def test_with_literal(q):
+    assert str(q("x").endswith("oo")) == "x.endswith('oo')"
+
+
+def test_lt(q):
+    assert str(q(10).Lt(q("x"))) == "(10 < x)"
+
+
+def test_lt2(q):
+    # TODO: 10 < x < 100
+    assert str(q(10).Lt(q("x")).Lt(100)) == "((10 < x) < 100)"
+
+
+def test_attr(q):
+    assert str(q("x").foo.bar(q("z"), "xxx").boo()) == "x.foo.bar(z, 'xxx').boo()"
+
+
+def test_items(q):
+    assert (
+        str(q("d")["gender"].In(["male", "female", "not-known"]))
+        == "(d['gender'] in ['male', 'female', 'not-known'])"
+    )
+
+
+def test_complex(q):
+    assert (
+        str(q("x").Add("y").neg().add(q("int")(True))) == "(- (x + y)).add(int(True))"
+    )
