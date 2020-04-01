686dfa1e1538963ebbf887ef713c503135595c27
podhmo <ababjam61+github@gmail.com>
2020-04-02 08:11:12 +0900

    feat: with annotations

--- baku/codegen/emit.py
+++ baku/codegen/emit.py
@@ -8,38 +8,64 @@ class Module(PythonModule, CodeObjectModuleMixin):
     pass
 
 
-def get_name_map(result: Result, *, name: str):
-    annotations = generate_annotations(result, toplevel_name=name)
-    return {k: v.get("after", v["before"])["name"] for k, v in annotations.items()}
+AnnotationMap = t.Dict[str, t.Any]  # {"": {"before": {"name": ""}, "after": {"name": ""}}}
+
+
+def get_name_map(
+    result: Result, *, name: str, annotations: t.Optional[AnnotationMap] = None
+):
+    default_annotations = generate_annotations(result, toplevel_name=name)
+    name_map = {
+        k: v.get("after", v["before"])["name"] for k, v in default_annotations.items()
+    }
+    if annotations is not None:
+        name_map.update(
+            {k: v.get("after", v["before"])["name"] for k, v in annotations.items()}
+        )
+    return name_map
 
 
 def to_graphql_type(
-    info: TypeInfo, *, mapping: t.Dict[t.Type[t.Any], t.Type[t.Any]], g: Symbol
+    info: TypeInfo,
+    *,
+    type_map: t.Dict[t.Type[t.Any], t.Type[t.Any]],
+    name_map: t.Dict[str, str],
+    g: Symbol
 ) -> t.Any:
     if hasattr(info, "base") and info.base is t.Optional:
-        return mapping[info.item.type]
-    return g.GraphQLNonNull(mapping[info.type])
+        return type_map[info.item.type]
+
+    if hasattr(info, "type"):
+        return g.GraphQLNonNull(type_map[info.type])
+    else:
+        return Symbol(name_map["/".join(info.path)])  # xxx
 
 
-def emit(result: Result, *, m: t.Optional[Module] = None) -> Module:
+def emit(
+    result: Result,
+    *,
+    m: t.Optional[Module] = None,
+    annotations: t.Optional[AnnotationMap] = None
+) -> Module:
     m = m or Module()
     m.toplevel = m.submodule()
     g = m.toplevel.import_("graphql", as_="g")
     m.sep()
 
-    mapping = {
+    type_map = {
         str: g.GraphQLString,
         int: g.GraphQLInt,
         bool: g.GraphQLBool,
     }
 
-    name_map = get_name_map(result, name="Person")
-    type_map = {name: g.symbol(name) for name in name_map.values()}
+    name_map = get_name_map(result, name="Person", annotations=annotations)
 
+    # todo: use lazy string
     for info in result.history:
         if not isinstance(info, Object):
             continue
-        name = name_map["/".join(info.path)]
+
+        name = name_map["/".join(info.path)]  # xxx
         m.stmt("{} = {}(", name, g.GraphQLObjectType)
         with m.scope():
             m.stmt("{!r},", name)
@@ -49,7 +75,11 @@ def emit(result: Result, *, m: t.Optional[Module] = None) -> Module:
                     m.stmt(
                         "{!r}: {},",
                         fieldname,
-                        g.GraphQLField(to_graphql_type(field, mapping=mapping, g=g)),
+                        g.GraphQLField(
+                            to_graphql_type(
+                                field, type_map=type_map, name_map=name_map, g=g
+                            )
+                        ),
                     )
             m.stmt("}")
         m.stmt(")")
---
+++ examples/codegen/02with-annotation/emit.py
@@ -0,0 +1,13 @@
+from baku.codegen.detect import detect
+from baku.codegen.emit import emit
+
+father = {"name": "foo", "age": 40}
+mother = {"name": "bar", "age": 40}
+me = {"name": "boo", "age": 20, "father": father, "mother": mother}
+result = detect(me)
+
+annotations = {
+    "": {"before": {"name": "Person"}, "after": {"name": "WPerson"}},
+    "father": {"before": {"name": "Father"}, "after": {"name": "Person"}},
+}
+print(emit(result, annotations=annotations))
